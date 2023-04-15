99739fc6694f8cdfe989a685c8f15bea066f4915
mentorvikash <mentorvikash@gmail.com>
2023-04-15 23:53:37 +0530

    add products array and display in Ui

--- src/routes/stateDemo/index.tsx
+++ src/routes/stateDemo/index.tsx
@@ -1,20 +1,27 @@
 import { component$, useSignal, useStore } from '@builder.io/qwik';
- 
+
 export default component$(() => {
 
     const fname = useSignal("vikash")
     const lname = useSignal("singh")
-    const fullName =  useStore({fname: "revive", lname: "coding"})
-    
+    const fullName = useStore({ fname: "revive", lname: "coding" })
+    const products = useStore([
+        { id: 0, name: "product1", price: 100, quantity: 10 },
+        { id: 1, name: "product2", price: 200, quantity: 20 },
+        { id: 2, name: "product3", price: 300, quantity: 30 },
+        { id: 3, name: "product4", price: 400, quantity: 40 }
+    ])
 
-  return (
+    return (
+        <div>
+            <div>
+                My name is {fullName.fname} {fullName.lname}
+            </div>
+            {products.map((product) => <p key={product.id} > {product.name} </p>)}
 
-    <div>
-    <div>
-        My name is {fullName.fname} {fullName.lname}
-    </div>
-    <button onClick$={() => fullName.fname = "Rajat" } >Change me</button>
-    </div>
+            <button onClick$={() => fullName.fname = "Rajat"} >Change me</button>
 
-  );
+        </div>
+
+    );
 });
