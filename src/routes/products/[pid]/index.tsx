423deda15022527a44ec213a14ea761485d2f1b9
mentorvikash <mentorvikash@gmail.com>
2023-04-19 00:29:15 +0530

    configue tailwind to project

--- src/routes/products/[pid]/index.tsx
+++ src/routes/products/[pid]/index.tsx
@@ -1,22 +1,28 @@
 import { component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 
-export const productData = routeLoader$(async ( requestEvent ) =>{
+export const productData = routeLoader$(async (requestEvent) => {
     const productID = requestEvent.params.pid
     const response = await fetch(`http://localhost:4000/products/${productID}`)
-    const data = await response.json() 
-    return data    
+    const data = await response.json()
+
+    if (response.status !== 200){
+        requestEvent.redirect(301,'/')
+    }else {
+        return data
+    }
+  
 })
 
 export default component$(() => {
 
-    const pdata = productData() 
+    const pdata = productData()
 
-  return( 
-  <div>
-    <p>this is our id {pdata.value.id}</p>
-    <p>this is our name {pdata.value.name}</p>
-    <p>this is our price {pdata.value.price}</p>
-  </div>
-  )
+    return (
+        <div>
+            <p class=" font-bold text-slate-200 pl-5 m-2 border rounded-md" >this is our id {pdata.value.id}</p>
+            <p>this is our name {pdata.value.name}</p>
+            <p>this is our price {pdata.value.price}</p>
+        </div>
+    )
 });
